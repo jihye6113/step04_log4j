f3e1a1939fde2f0ff0c5aa7acf92f54edbb0f6c2
kosta <kosta@192.168.0.184>
2020-05-14 18:19:09 +0900

    step04_log4j commit (20-05-14)

---
+++ src/main/java/kosta/exam/MainApp.java
@@ -0,0 +1,12 @@
+package kosta.exam;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		LogTest test = new LogTest();
+		test.log();
+		
+		System.out.println("**파일을 확인해보아요**");
+	}
+
+}
